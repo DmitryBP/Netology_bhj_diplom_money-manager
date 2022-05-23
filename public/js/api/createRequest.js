881973d867c2a8ef7512202e7c154f11c0fa667e
Dmitry_home <79811924115@yandex.ru>
2022-05-23 22:14:51 +0300

    createRequest v. 1

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -6,6 +6,18 @@ const createRequest = (options = {}) => {
   const xhr = new XMLHttpRequest;
   
   if (options.method == 'GET') {
+    /* Не могу разобраться куда отправлять запрос в примере https://exemple.com
+    в ДЗ давались конкретные ссылки здесь в описании такой ссылки не нашел
+     */
+    xhr.open('GET', `https://exemple.com?mail=${options.data.mail}&password=${options.data.password}`);
+    xhr.send();
+  } else {
+    const xhr = new XMLHttpRequest,
+      formData = new FormData;
+      formData.append('mail', options.data.mail);
+      formData.append('password', options.data.password);
 
+      xhr.open('POST', 'https://exemple.com');
+      xhr.send(formData)
   }
 };
